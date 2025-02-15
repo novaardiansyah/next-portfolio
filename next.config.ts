e5c66619e25487d0832fce52ce803eb25a5253f5
novaardiansyah <novaardiansyah78@gmail.com>
2025-02-16 02:08:30 +0700

    clear cache and set allowed images

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "https://admin.novaardiansyah.my.id/",
+        hostname: "admin.novaardiansyah.my.id",
       },
     ],
   },
