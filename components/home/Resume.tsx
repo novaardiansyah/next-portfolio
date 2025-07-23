13343a31aec2e6cba8833de9a9e2ddce7caf70a3
novaardiansyah <novaardiansyah78@gmail.com>
2025-07-23 20:15:57 +0700

    Update Resume component for improved clarity and structure

--- components/home/Resume.tsx
+++ components/home/Resume.tsx
@@ -42,15 +42,15 @@ function Resume() {
           <div className="col-lg-6" data-aos="fade-up" data-aos-delay="100">
             <h3 className="resume-title">Professional Experience</h3>
             <div className="resume-item">
-              <h4>Junior Web and Mobile Development</h4>
+              <h4>Senior Web Developer</h4>
               <h5>2021 - Present</h5>
               <p><em>SofwareHouse, Tangerang, Indonesia.</em></p>
               <ul>
-                <li>Play a pivotal role in identifying and resolving complex backend and frontend challenges for clients, ensuring seamless application functionality and user satisfaction.</li>
-                <li>Take charge of application orchestration, overseeing its smooth operation for both clients and customers within the company.</li>
-                <li>Collaborate closely with cross-functional teams, directing the implementation of solutions and ensuring efficient problem resolution</li>
-                <li>Serve as a key liaison between clients and internal teams, facilitating effective communication and alignment of project goals.</li>
-                <li>Streamline application workflows and processes, enhancing overall user experiences and driving customer satisfaction.</li>
+                <li>Develop and maintain web applications, focusing on backend logic and API integration for the frontend (without UI/UX design).</li>
+                <li>Design, manage, and optimize database structures for secure and efficient data handling.</li>
+                <li>Troubleshoot and resolve technical issues, as well as address client feedback and new feature requests.</li>
+                <li>Coordinate and mentor team members to ensure effective project delivery.</li>
+                <li>Collaborate with clients and cross-functional teams to align solutions with business goals.</li>
               </ul>
             </div>
           </div>
